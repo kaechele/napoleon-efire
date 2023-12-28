d13300e05916375d9838a00b70a2736fa1073873
Felix Kaechele <felix@kaechele.ca>
2023-12-28 18:57:24 +0100

    fix: remove LED lights from unsupported features
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- custom_components/napoleon_efire/const.py
+++ custom_components/napoleon_efire/const.py
@@ -9,10 +9,9 @@ CONF_FEATURES = "features"
 LOCAL_NAME_PREFIX = "NAP_FPC_"
 
 # List of features supported by the bonaparte library but not by this component
-# - The external LED controller is unsupported at this moment but could be implemented
 # - The controller's timer feature is not supported and probably should not be implemented
 #   This feature can be achieved using automations from within Home Assistant instead
-UNSUPPORTED_FEATURES = ["led_lights", "timer"]
+UNSUPPORTED_FEATURES = ["timer"]
 
 # Coordinator constants
 UPDATE_INTERVAL = timedelta(seconds=30)
